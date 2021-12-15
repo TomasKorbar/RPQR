638e45268ad1deb7e3b600a70eae13f0755cf8c6
Tomas Korbar <tkorbar@redhat.com>
2021-12-15 15:09:53 +0100

    Print maintainers affected by orphaning of package

--- rpqr/app/main.py
+++ rpqr/app/main.py
@@ -13,19 +13,27 @@ if __name__ == "__main__":
     graph = loader.createDatabase()
     scnr = RPQRScanner(config)
     parser = RPQRParser(config)
-    #tokens = scnr.getTokens("WHATDEPENDSON 'libyang-1.0.184-2.fc33.x86_64' 2 & ~(NAMELIKE 'frr' | NAMELIKE 'devel')")
-    #tokens = scnr.getTokens("MAINTAINER 'mruprich' & DEPENDSONUSER 'tkorbar' 0")
-    #tokens = scnr.getTokens("MAINTAINER 'mruprich' & DEPENDSONUSER 'tkorbar' 0")
-    # VISUALIZE(MAINTAINER('mruprich') & DEPENDSONUSER('tkorbar',0)) & NAMELIKE('kappa')
-    tokens = scnr.getTokens("SUBSETNAMELIKE('cpp', SUBSETNAMELIKE('devel', NAMELIKE('libyang'))) & NAMELIKE('i686')")
-    AST = parser.parseTokens(tokens)
     interpreter = RPQRInterpreter(config)
-    
+    AST = parser.parseTokens(scnr.getTokens("MAINTAINER('orphan')"))
     result = interpreter.performCommands(graph, AST)
+    for node in list(result.nodes):
+        AST = parser.parseTokens(scnr.getTokens("WHATDEPENDSON('%s', 20)" % graph.nodes[node]["name"]))
+        dependentPackages = interpreter.performCommands(graph, AST)
+        maintainerList = []
+        for pkgId in list(dependentPackages.nodes):
+            for maintainer in dependentPackages.nodes[pkgId]["maintainer"]:
+                if maintainer not in maintainerList:
+                    maintainerList.append(maintainer)
+        maintainerList.remove('orphan')
+        print("%s => " % graph.nodes[node]["name"], end="")
+        for maint in maintainerList:
+            print("%s " % maint, end="")
+        print()
 
 
 
 
+    """
 
     labelDict = {}
     for nodeId in result:
@@ -34,4 +42,5 @@ if __name__ == "__main__":
     networkx.draw_spring(result, with_labels = True, labels=labelDict, )
 
     plt.show()
+    """
     print("finish")
