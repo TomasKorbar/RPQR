ef1bd633f5cec44cddf3ba0ada0180859cc44a54
Tomas Korbar <tkorbar@redhat.com>
2022-04-05 13:19:56 +0200

    More documentation

--- rpqr/query/RPQRQuery.py
+++ rpqr/query/RPQRQuery.py
@@ -4,10 +4,12 @@ Author: Tomáš Korbař (tomas.korb@seznam.cz)
 Copyright 2021 - 2022 Tomáš Korbař
 '''
 
+from tkinter.messagebox import NO
 from typing import Optional
 import networkx
 
 from rpqr.library import RPQRConfiguration
+from rpqr.loader import RPQRLoader
 from rpqr.query.language.interpreter import RPQRInterpreter
 from rpqr.query.language.parser import RPQRParser
 from rpqr.query.language.scanner import RPQRScanner
@@ -16,25 +18,25 @@ from rpqr.query.language.scanner import RPQRScanner
 class RPQRQuery:
     """ Interface for simplification of executing queries
     """
-    def performQuery(query: str, graph: networkx.MultiDiGraph, config: RPQRConfiguration) -> Optional[networkx.MultiDiGraph]:
-        """ perform query on graph
+    def __init__(self, config: RPQRConfiguration) -> None:
+        self._config = config
+        loader = RPQRLoader(self._config)
+        self._graph = loader.createDatabase()
 
+    def performQuery(self, query: str, graph: networkx.MultiDiGraph = None, config: RPQRConfiguration = None) -> Optional[networkx.MultiDiGraph]:
+        """ perform query on graph
         :param query: query in RPQR language
         :type query: str
-        :param graph: graph of packages
-        :type graph: networkx.MultiDiGraph
-        :param config: RPQR configuration 
-        :type config: RPQRConfiguration
         :return: result of query
         :rtype: Optional[networkx.MultiDiGraph]
         """
-        scanner = RPQRScanner(config)
-        parser = RPQRParser(config)
-        interpreter = RPQRInterpreter(config)
+        scanner = RPQRScanner(self._config)
+        parser = RPQRParser(self._config)
+        interpreter = RPQRInterpreter(self._config)
         tokens = scanner.getTokens(query)
         if tokens == None:
             return None
         abstractSyntacticTree = parser.parseTokens(tokens)
         if abstractSyntacticTree == None:
             return None
-        return interpreter.performCommands(graph, abstractSyntacticTree)
+        return interpreter.performCommands(self._graph, abstractSyntacticTree)
